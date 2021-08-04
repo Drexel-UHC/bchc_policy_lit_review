b46a8bbc484997106876c09b70e08ee9986857c3
rl627 <rl627@drexel.edu>
2021-08-04 15:46:09 -0400

    authentication fixed

--- Prototypes/PN_1/js/auth.js
+++ Prototypes/PN_1/js/auth.js
@@ -2,7 +2,7 @@ function formSubmit(event) {
   console.log(`Submit clicked; ${passwordUHC.value}`);
   if ( passwordUHC.value === "uhc") {
     console.log(`Correct`);
-    document.querySelector('#prototype').style.display = 'block';
+    document.querySelector('#app_container').style.display = 'block';
     document.querySelector('#form').style.display = 'none';
   } else {
       console.log(`INCORRECT`);
@@ -14,6 +14,5 @@ function formSubmit(event) {
 
 const form = document.getElementById('form');
 const log = document.getElementById('log');
-const userName = document.getElementById('userName');
 const passwordUHC = document.getElementById('passwordUHC');
 form.addEventListener('submit', formSubmit);
